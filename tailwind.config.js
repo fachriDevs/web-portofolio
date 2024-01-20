ee0c69f8ae59d83d163c0e15e8bfb5c995ef2078
fachriDevs <muhamadfachrifasya@gmail.com>
2024-01-20 18:52:19 +0700

    menambahkan repo baru

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/index.html'
+  ],
+  theme: {
+    extend: {
+      letterSpacing:{
+        renggang: '.5em',
+        sempit: '-0.5em'
+      }
+    },
+  },
+  plugins: [],
+}
+
